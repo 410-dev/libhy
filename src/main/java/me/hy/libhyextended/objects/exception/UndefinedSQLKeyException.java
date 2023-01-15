7436c54105789d6a92cfefbb1ecab4ef1f0f3fc7
410 <58301450+410-dev@users.noreply.github.com>
2023-01-15 14:57:00 -0500

    Update UndefinedSQLKeyException.java

--- src/main/java/me/hy/libhyextended/objects/exception/UndefinedSQLKeyException.java
+++ src/main/java/me/hy/libhyextended/objects/exception/UndefinedSQLKeyException.java
@@ -1,6 +1,6 @@
 package me.hy.libhyextended.objects.exception;
 
-public class UndefinedSQLKeyException extends Exception {
+public class UndefinedSQLKeyException extends RuntimeException {
     public UndefinedSQLKeyException(String key) {
         super("The required SQL key " + key + " is undefined.");
     }
