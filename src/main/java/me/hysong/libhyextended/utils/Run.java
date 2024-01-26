1afb50008a0b1097fc9266f4f92ce816b40beaa9
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-01-26 18:05:34 +0900

    async with delay, and sync / sync with delay

--- src/main/java/me/hysong/libhyextended/utils/Run.java
+++ src/main/java/me/hysong/libhyextended/utils/Run.java
@@ -4,4 +4,28 @@ public class Run {
     public static void async(Runnable runnable) {
         new Thread(runnable).start();
     }
+
+    public static void async(int delayMs, Runnable runnable) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(delayMs);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            async(runnable);
+        }).start();
+    }
+
+    public static void sync(Runnable runnable) {
+        runnable.run();
+    }
+
+    public static void sync(int delayMs, Runnable runnable) {
+        try {
+            Thread.sleep(delayMs);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sync(runnable);
+    }
 }
